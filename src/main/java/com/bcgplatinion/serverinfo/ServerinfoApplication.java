7e12d9b62436ba70122960ced33c7cf9ae3e4d5f
Aurélien Christman <christman.aurelien@bcgplatinion.com>
2020-03-31 17:00:43 +0200

    Reading env vars

--- src/main/java/com/bcgplatinion/serverinfo/ServerinfoApplication.java
+++ src/main/java/com/bcgplatinion/serverinfo/ServerinfoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.json.*;
 
 @SpringBootApplication
 @RestController
@@ -19,7 +20,12 @@ public class ServerinfoApplication {
 		String env = System.getenv("VCAP_SERVICES");
 		if (env == null) return ("Error : not running on Cloud Foundry environment");
 
-		return "ok";
+		JSONObject cfVars = new JSONObject(env);
+		String space = cfVars.getString("space_name");
+		String organization = cfVars.getString("organization_name");
+		String appName = cfVars.getString("application_name");
+
+		return String.format("<p>Organization name : %s</p><p>Space name : %s</p><p>Application name : %s</p>", organization, space, appName);
 	}
 	
 	@GetMapping("/hello")
